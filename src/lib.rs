779989c7da0042efb55e21dd0bf831fdc1e1906f
jean020889 <149705729+jean020889@users.noreply.github.com>
2026-05-22 23:12:07 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use pqcrypto_kyber::kyber512::{keypair, PublicKey, SecretKey};
 
 #[no_mangle]
-pub extern "C" fn generar_llaves() {
-    println!("Motor PQC: Generando llaves seguras...");
+pub extern "C" fn generar_llaves_pqc() -> *mut (Vec<u8>, Vec<u8>) {
+    let (pk, sk) = keypair();
+    // Empaquetamos en un Box para que la memoria persista fuera de Rust
+    let resultado = Box::new((pk.as_bytes().to_vec(), sk.as_bytes().to_vec()));
+    Box::into_raw(resultado)
+}
+
+#[no_mangle]
+pub extern "C" fn liberar_llaves(ptr: *mut (Vec<u8>, Vec<u8>)) {
+    unsafe { Box::from_raw(ptr) };
 }
